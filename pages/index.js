ff68e9330b6d4b1e256fd945812980b43f1e2c27
Mark Anthony <mamosomarkanthony@gmail.com>
2023-04-23 13:52:20 +0800

    Initial Commit

--- pages/index.js
+++ pages/index.js
@@ -1,124 +1,256 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Box, Stack, StackDivider, Container, Heading, Image, VStack, HStack, Button, IconButton} from "@chakra-ui/react";
+import { Card,CardHeader, CardBody, Text, Flex, List, ListItem, ListIcon, MdCheckCircle } from '@chakra-ui/react';
+import Head from "next/head";
+import { 
+  FaFacebook, 
+  FaTwitter, 
+  FaLinkedin, 
+  FaGithub,
+  FaNodeJs,
+  FaNpm,
+  FaReact,
+  FaAngular,
+  FaHtml5,
+  FaJs,
+  FaPhp,
+  FaWordpress,
+  FaBlackTie
+} from 'react-icons/fa';
+import { EmailIcon, HamburgerIcon, AddIcon,  } from "@chakra-ui/icons";
+import { Link } from "@chakra-ui/react";
+import { 
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem
 
-const inter = Inter({ subsets: ['latin'] })
+} from "@chakra-ui/react";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+export default function Home(){
+  return(
+    <Box >
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <Head>
+        <title>My Portfolio</title>
+        <link href="favicon.ico" rel="icon" sizes="32x32" type="image/icon"/>
+      </Head>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+      <header>
+        <Box boxShadow='sm' position='relative' bgGradient='linear(to-l, #7928CA, #FF0080)'>
+          <Flex justifyContent='center'>
+            <Box position='absolute' left={0} top='50%' transform='translateY(-50%)' zIndex={4}>
+              <Menu>
+                <MenuButton
+                  height='14'
+                  width='14'
+                  as={IconButton}
+                  m={5}
+                  fontSize='30px'
+                  aria-label='Options'
+                  icon={<HamburgerIcon />}
+                  variant='ghost'
+                />
+                <MenuList>
+                <MenuItem as='a' fontSize={14} href='mamoso_mark_anthony_v.pdf' icon={<FaBlackTie size='2em' />} minH='48px'>
+                    My Resume
+                  </MenuItem>
+                  
+                </MenuList>
+              </Menu>
+            </Box>
+            <Text
+              textAlign='center'
+              fontSize='6xl'
+              fontWeight='extrabold'
+              color='white'
+            >
+              My Portfolio
+            </Text>
+          </Flex>
+        </Box>
+      </header>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <Flex flexDirection={['column', 'column','row']} justifyContent='center'>
+        
+          <Card m={5}>
+            <CardBody>
+              <Container  centerContent>
+            
+                  <VStack
+                    align="center"
+                  >
+                    <HStack 
+                      bgGradient='linear(to-l, #7928CA, #FF0080)'
+                      backgroundSize='cover' 
+                      w='100%' 
+                      justifyContent='center'
+                      borderRadius='full'
+                      
+                      >
+                        <HStack
+                        bgColor='#ffffffAA'
+                        borderRadius='full'
+                          w='100%'
+                          justifyContent='end'
+                          m={2}
+                        >
+                    <HStack justifyContent='center'>
+                      <Heading
+                        as='h1'
+                        size="4xl"
+                        bgGradient='linear(to-l, #7928CA, #FF0080)'
+                        bgClip='text'
+                        textAlign='center'
+                        noOfLines={1}
+                        >Caught!</Heading>
+                    </HStack>
+                          <Box m={2}>
+                            <Image
+                            m={2}
+                            p={2}
+                            borderRadius='full'
+                            boxSize='150px'
+                            src='profile.jpg'
+                            alt='Mark Anthony V. Mamoso'
+                            border='dotted 8px #7928CA'
+                            
+                            
+                                                    />
+                          </Box>
+                        </HStack>
+
+                    </HStack>
+                    
+
+                    
+                    <Box>
+                      <Text fontSize='2xl' m={5} align='center'>
+                        Hi, I'm Mark Anthony Mamoso - Full Stack Web Developer in the Philippines
+                        with expertise in front-end and back-end web development, as well as database design and management.
+                        I specialize in creating robust, scalable web applications that are optimized for speed, performance and user experience.
+                      </Text>
+                    </Box>
+
+                    
+                  </VStack>
+
+            
+              </Container>
+            </CardBody>
+          </Card>
+        
+
+        
+          <Box flexGrow={1}>
+            <Card m={5}>
+              <CardHeader>
+                <Heading size='md'>Key Skills</Heading>
+              </CardHeader>
+
+              <CardBody>
+                <Stack divider={<StackDivider />} spacing='4'>
+                  <Box>
+                    <Heading size='xs' textTransform='uppercase'>
+                      Font-End
+                    </Heading>
+                    <Text pt='2' fontSize='sm'>
+                      HTML/CSS, JavaScript, React.js, Angular
+                    </Text>
+                  </Box>
+                  <Box>
+                    <Heading size='xs' textTransform='uppercase'>
+                      Back-End
+                    </Heading>
+                    <Text pt='2' fontSize='sm'>
+                      JavaScript, Node.js, PHP, MySQL
+                    </Text>
+                  </Box>
+                  <Box>
+                    <Heading size='xs' textTransform='uppercase'>
+                      Tools
+                    </Heading>
+                    <Text pt='2' fontSize='sm'>
+                      NPM, Git, VS Code, Terminal
+                    </Text>
+                  </Box>
+                </Stack>
+              </CardBody>
+            </Card>
+
+
+            <Card m={5}>
+              <CardHeader>
+                <Heading size='md'>Keep In Touch</Heading>
+              </CardHeader>
+
+              <CardBody>
+                <HStack>
+                  <Link href='https://www.linkedin.com/in/mark-anthony-mamoso-6b26b5171/' target='_blank'><IconButton variant='solid' fontSize='25px' colorScheme='linkedin' icon={<FaLinkedin />} size='sm' /></Link>
+                  <IconButton variant='solid' fontSize='25px' colorScheme='teal' icon={<EmailIcon />} size='sm' />
+                  <Text fontSize='20px'>
+                    mamosomarkanthony@gmail.com
+                </Text>
+                  
+                  
+                </HStack>
+                
+
+              
+              </CardBody>
+            </Card>
+          </Box>
+        
+
+      </Flex>
+
+      <Box>
+        <Card m={5}>
+        <CardHeader>
+                <Heading size='3xl' textAlign='center'>Technologies</Heading>
+              </CardHeader>
+          <CardBody>
+
+            <HStack spacing='5px' justify='center'>
+              <Box>
+                <FaGithub size='4em' />
+              </Box>
+              <Box>
+                <FaNodeJs size='4em' />
+              </Box>
+              <Box>
+                <FaNpm size='4em' />
+              </Box>
+              <Box>
+                <FaReact size='4em' />
+              </Box>
+              <Box>
+                <FaAngular size='4em' />
+              </Box>
+              <Box>
+                <FaHtml5 size='4em' />
+              </Box>
+              <Box>
+                <FaJs size='4em' />
+              </Box>
+              <Box>
+                <FaPhp size='4em' />
+              </Box>
+
+              <Box>
+                <FaWordpress size='4em' />
+              </Box>
+            </HStack>
+
+          </CardBody>
+        </Card>
+      </Box>
+      
+      
+
+
+      
+    </Box>
+  );
 }
